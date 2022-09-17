0b327f9bd89d79aefee0c6bbfac02a1df171947d
Natasanl <112381794+Natasanl@users.noreply.github.com>
2022-09-17 20:14:18 +0200

    finished assignment

---
+++ Labb1Natasa/Labb1Natasa/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input;
+long sum = 0;
+Regex regex = new Regex(@"\d");
+
+if (args.Length > 0)
+{
+    input = args[0];
+}
+else
+{
+    Console.WriteLine("Skriv in en text med tal och tecken");
+    input = Console.ReadLine();
+}
+
+bool IsNumber(char character)
+{
+    return regex.IsMatch(char.ToString(character));
+}
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (IsNumber(input[i]))
+    {
+        for (int j = i + 1; j < input.Length; j++)
+        {
+            if (input[i] == input[j])
+            {
+                string substring = input.Substring(i, j - i + 1);
+                sum += long.Parse(substring);
+
+                Console.WriteLine();
+                Console.Write(input[..i]);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(substring);
+                Console.ResetColor();
+                Console.WriteLine(input[(j + 1)..]);
+
+                break;
+            }
+            else if (!IsNumber(input[j]))
+            {
+                break;
+            }
+        }
+    }
+}
+Console.WriteLine($"\nTotal: {sum}");
